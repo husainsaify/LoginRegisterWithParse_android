0c5ea219614968c8101ed46aac03e55215356f80
husainsaify <hackerkernel15@gmail.com>
2016-01-18 12:35:57 +0530

    Completed logout in homeActivity

--- app/src/main/java/com/hackerkernel/loginregisterwithparse/HomeActivity.java
+++ app/src/main/java/com/hackerkernel/loginregisterwithparse/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.hackerkernel.loginregisterwithparse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.parse.ParseUser;
@@ -24,7 +27,7 @@ public class HomeActivity extends AppCompatActivity {
         TextView fullname = (TextView) findViewById(R.id.homeFullname);
         TextView email = (TextView) findViewById(R.id.homeEmail);
         TextView phone = (TextView) findViewById(R.id.homePhone);
-
+        Button logout = (Button) findViewById(R.id.logout);
 
         //get logged in user
         ParseUser currentUser = ParseUser.getCurrentUser();
@@ -33,5 +36,17 @@ public class HomeActivity extends AppCompatActivity {
         email.setText(currentUser.getEmail());
         phone.setText(currentUser.getString("phone"));
 
+        //When logout button is clicked logout the user and send him to mainActivity
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                //send user to MainActivity
+                Intent intent = new Intent(getApplication(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
